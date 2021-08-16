34d5e4427c61222c20fcedfab717407fa3943432
Devin Spikowski <vegeta897@gmail.com>
2021-08-16 04:02:06 -0400

    :bug: Fix "Not" query bug caused by terser

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,20 @@ const { merge } = require('webpack-merge')
 const ZipPlugin = require('zip-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const ImageminPlugin = require('imagemin-webpack-plugin').default
+const TerserPlugin = require('terser-webpack-plugin')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
 	mode: 'production',
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					keep_fnames: /^Query/,
+				},
+			}),
+		],
+	},
 	plugins: [
 		new CleanWebpackPlugin(),
 		new ImageminPlugin({
