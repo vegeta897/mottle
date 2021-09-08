2858db2cfd95372b1c12c0760cdba077cf36c7eb
Devin Spikowski <vegeta897@gmail.com>
2021-09-08 19:46:29 -0400

    :fire: Remove old line paint code

--- src/pixi/object_manager.ts
+++ src/pixi/object_manager.ts
@@ -1,7 +1,5 @@
 import type { DisplayObject } from 'pixi.js'
-import { Vector2 } from '../util'
 import { Container, Graphics } from 'pixi.js'
-import * as PIXI from 'pixi.js'
 import { PixiApp } from './pixi_app'
 
 export const DisplayObjects: DisplayObject[] = [] // Indexed by entity ID
@@ -20,33 +18,3 @@ axis.lineStyle({ width: 0 })
 axis.drawRect(0, 0, 1, 1)
 axis.y = PixiApp.shared.viewport.worldScreenHeight / 2
 splatContainer.addChild(axis)
-
-let currentLine: Graphics | null = null
-let currentLinePoints = 0
-let lastPoint: Vector2 | null = null
-
-export function paintLine(
-	{ x, y }: Vector2,
-	newLine: boolean,
-	thickness: number
-) {
-	if (newLine || currentLinePoints > 16 || !currentLine) {
-		if (newLine || !currentLine) lastPoint = { x, y }
-		currentLine = new Graphics()
-		splatContainer.addChild(currentLine)
-		currentLinePoints = 0
-	} else if (
-		Vector2.getMagnitude({ x: x - lastPoint!.x, y: y - lastPoint!.y }) > 6
-	) {
-		currentLine.moveTo(lastPoint!.x, lastPoint!.y)
-		currentLine.lineStyle({
-			width: 4 + 8 * thickness,
-			color: 0xff88aa,
-			cap: PIXI.LINE_CAP.ROUND,
-			join: PIXI.LINE_JOIN.ROUND,
-		})
-		currentLine.lineTo(x, y)
-		currentLinePoints++
-		lastPoint = { x, y }
-	}
-}
